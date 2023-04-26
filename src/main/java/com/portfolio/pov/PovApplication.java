ab0c27850ddf6a492fd15bad84e2fe1afebc6dad
Wedo Pictures <simja.82@hotmail.com>
2023-04-26 16:22:27 -0300

    Proyecto de Spring Boot

---
+++ src/main/java/com/portfolio/pov/PovApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.pov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PovApplication.class, args);
+	}
+
+}
