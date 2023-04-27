6a0fa9421517a9b3bd00cd3ccc26065f52461659
Wedo Pictures <simja.82@hotmail.com>
2023-04-27 09:55:49 -0300

    CrossOrigin Local

--- src/main/java/com/portfolio/pov/controllers/AccountController.java
+++ src/main/java/com/portfolio/pov/controllers/AccountController.java
@@ -6,7 +6,7 @@ import com.portfolio.pov.services.AccountService;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 //@CrossOrigin(origins="*", allowedHeaders="*")
 @RestController
 //@CrossOrigin(origins = "https://pov-portfolio.web.app/")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("account")
 public class AccountController {
     
--- src/main/java/com/portfolio/pov/controllers/SoftController.java
+++ src/main/java/com/portfolio/pov/controllers/SoftController.java
@@ -5,7 +5,7 @@ import com.portfolio.pov.services.SoftService;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 //@CrossOrigin(origins = "https://pov-portfolio.web.app/")
 @RequestMapping("soft")
 public class SoftController {
